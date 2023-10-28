15c307f1ca645a4cfcca344e91bf96b42abbe263
binioosini <binioosini@gmail.com>
2023-10-28 21:29:27 +0300

    one

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Navbar from './Navbar'
 import Container from './Container'
 import Services from './Services'
 import Projects from './Projects'
-import Tools from './Tools'
 
 export default function Home() {
   return (
@@ -16,10 +15,8 @@ export default function Home() {
       <Services/>
       <div id='projects' className='box w-10/12 mx-auto max-md:mt-[-50px] mt-8'></div>
       <Projects/>
-      <div id='tools' className='box w-10/12 mx-auto max-md:mt-[-50px] mt-8'></div>
-      <Tools/>
       <div className='w-10/12 mx-auto my-10'>
-        <div className='flex items-start text-2xl text-neutral-300'><p className='text-5xl mr-2 mt-[2px]'>&#169;</p> Perfect Code <small className='mt-[2px] ml-2'>2023</small> </div>
+        <div className='flex items-start text-2xl text-neutral-300'><p className='text-5xl mr-2 mt-[2px]'>&#169;</p> zbir<small className='mt-[2px] ml-2'>2023</small> </div>
       </div>
     </div>
   )
